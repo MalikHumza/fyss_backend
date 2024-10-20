18cd8b96370d921d765a049dfe532c376e71b1a2
MalikHumza <humzamalik1146@gmail.com>
2024-10-21 01:00:24 +0500

    feat: create-support-plan for students

--- src/data/services/support.service.ts
+++ src/data/services/support.service.ts
@@ -1,4 +1,5 @@
 import database from "@config/database";
+import { CreateSupportPlanDTO } from "@data/dtos/supportPlan/create_support_plan.dto";
 import { QUARTER_MONTHS } from "@prisma/client";
 import { Service } from "typedi";
 
@@ -28,6 +29,16 @@ export class SupportPlanService {
         student_id,
         student_email,
       },
+    });
+  }
+
+  createSupportPlanForStudent(staff_id: string, student_id: string, data: CreateSupportPlanDTO) {
+    return this.support.create({
+      data: {
+        student_id,
+        staff_id,
+        ...data
+      }
     });
   }
 }
---
+++ src/domain/usecases/supportPlan/create_support_plan_for_student.ts
@@ -0,0 +1,30 @@
+import { CreateSupportPlanDTO } from "@data/dtos/supportPlan/create_support_plan.dto";
+import { RequestWithUser } from "@data/interfaces/request.interface";
+import { HttpResponse } from "@data/res/http_response";
+import { SupportPlanService } from "@data/services/support.service";
+import { DateToMiliSeconds } from "@infrastructure/common/epoch-converter";
+import { Inject, Service } from "typedi";
+
+@Service()
+export class CreateSupportPlanForStudentUseCase {
+    @Inject()
+    private supportPlanService: SupportPlanService;
+
+    public async call(req: RequestWithUser, student_id: string, data: CreateSupportPlanDTO) {
+        const staff_id = req.user.id;
+        const result = await this.supportPlanService.createSupportPlanForStudent(staff_id, student_id, data);
+        const response = {
+            id: result.id,
+            staff_id,
+            student_id,
+            student_email: result.student_email,
+            strengths: result.strengths,
+            area_of_development: result.area_of_development,
+            current_strategy_and_support: result.current_strategy_and_support,
+            month: result.month,
+            year: result.year,
+            created_at: DateToMiliSeconds(result.createdAt)
+        }
+        return new HttpResponse(response, false);
+    }
+}

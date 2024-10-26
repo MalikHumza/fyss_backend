13d91b0346257d74504b68d3213a03afb0a929d1
MalikHumza <humzamalik1146@gmail.com>
2024-10-26 16:23:19 +0500

    chore: format

--- src/domain/usecases/properties/get_properties_by_staff.ts
+++ src/domain/usecases/properties/get_properties_by_staff.ts
@@ -5,21 +5,22 @@ import { Inject, Service } from "typedi";
 
 @Service()
 export class GetPropertiesByStaffUseCase {
-    @Inject()
-    private propertiesService: PropertiesService;
+  @Inject()
+  private propertiesService: PropertiesService;
 
-    public async call(req: RequestWithUser) {
-        const staff_id = req.user.id;
-        const result = await this.propertiesService.getPropertiesByStaffId(staff_id);
-        const response = result.map(i => ({
-            id: i.id,
-            staff_id: i.staff_id,
-            name: i.name,
-            image: i.image,
-            location: i.location,
-            occupancy: i.occupancy,
-            description: i.description,
-        }));
-        return new HttpResponse(response, false);
-    }
+  public async call(req: RequestWithUser) {
+    const staff_id = req.user.id;
+    const result =
+      await this.propertiesService.getPropertiesByStaffId(staff_id);
+    const response = result.map((i) => ({
+      id: i.id,
+      staff_id: i.staff_id,
+      name: i.name,
+      image: i.image,
+      location: i.location,
+      occupancy: i.occupancy,
+      description: i.description,
+    }));
+    return new HttpResponse(response, false);
+  }
 }

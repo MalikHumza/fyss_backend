2a81b77143df55abf2ed5f6b5293bd5efea3e589
MalikHumza <humzamalik1146@gmail.com>
2024-11-03 20:47:17 +0500

    chore: format

--- src/data/services/alcohal_and_drug_logs.service.ts
+++ src/data/services/alcohal_and_drug_logs.service.ts
@@ -3,14 +3,13 @@ import { Service } from "typedi";
 
 @Service()
 export class AlcohalAndDrugLogsService {
-    public alcohal = database.instance.alcohalAndDrugsLogIncident;
+  public alcohal = database.instance.alcohalAndDrugsLogIncident;
 
-    getAlcohalAndDrugLogsIncidentsByProperty(property_id: string) {
-        return this.alcohal.findMany({
-            where: {
-                property_id,
-            }
-        })
-    }
-
+  getAlcohalAndDrugLogsIncidentsByProperty(property_id: string) {
+    return this.alcohal.findMany({
+      where: {
+        property_id,
+      },
+    });
+  }
 }
--- src/infrastructure/controllers/incidents.controller.ts
+++ src/infrastructure/controllers/incidents.controller.ts
@@ -3,13 +3,13 @@ import { GetAlcohalAndDrugsIncidentUseCase } from "@domain/usecases/incidents/al
 import { GetMissingFromCareIncidentUseCase } from "@domain/usecases/incidents/missing_from_care_incident";
 import { CheckTokenExpiry } from "@infrastructure/middlewares/token_expiry.middleware";
 import {
-    Authorized,
-    Get,
-    HttpCode,
-    JsonController,
-    Param,
-    Req,
-    UseBefore,
+  Authorized,
+  Get,
+  HttpCode,
+  JsonController,
+  Param,
+  Req,
+  UseBefore,
 } from "routing-controllers";
 import Container from "typedi";
 
@@ -17,28 +17,28 @@ import Container from "typedi";
 @UseBefore(CheckTokenExpiry)
 @Authorized()
 export class IncidentsController {
-    private getAlcohalAndDrugsIncidentUseCase = Container.get(
-        GetAlcohalAndDrugsIncidentUseCase,
-    );
-    private getMissingFromCareIncidentUseCase = Container.get(
-        GetMissingFromCareIncidentUseCase,
-    );
+  private getAlcohalAndDrugsIncidentUseCase = Container.get(
+    GetAlcohalAndDrugsIncidentUseCase,
+  );
+  private getMissingFromCareIncidentUseCase = Container.get(
+    GetMissingFromCareIncidentUseCase,
+  );
 
-    @Get("/drugs/:property_id")
-    @HttpCode(200)
-    getAlcohalAndDrugsIncidentByProperty(
-        @Req() req: RequestWithUser,
-        @Param("property_id") property_id: string,
-    ) {
-        return this.getAlcohalAndDrugsIncidentUseCase.call(req, property_id);
-    }
+  @Get("/drugs/:property_id")
+  @HttpCode(200)
+  getAlcohalAndDrugsIncidentByProperty(
+    @Req() req: RequestWithUser,
+    @Param("property_id") property_id: string,
+  ) {
+    return this.getAlcohalAndDrugsIncidentUseCase.call(req, property_id);
+  }
 
-    @Get("/missing-care/:property_id")
-    @HttpCode(200)
-    getMissingFromCareByStaffAndProperty(
-        @Req() req: RequestWithUser,
-        @Param("property_id") property_id: string,
-    ) {
-        return this.getMissingFromCareIncidentUseCase.call(req, property_id);
-    }
+  @Get("/missing-care/:property_id")
+  @HttpCode(200)
+  getMissingFromCareByStaffAndProperty(
+    @Req() req: RequestWithUser,
+    @Param("property_id") property_id: string,
+  ) {
+    return this.getMissingFromCareIncidentUseCase.call(req, property_id);
+  }
 }

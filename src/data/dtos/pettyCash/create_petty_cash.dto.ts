1f901ffe190e2ef795daab1544fde2d20ff92c94
MalikHumza <humzamalik1146@gmail.com>
2024-11-04 05:37:18 +0500

    feat: financial-petty-cash

---
+++ src/data/dtos/pettyCash/create_petty_cash.dto.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Service } from "typedi";
+
+@Service()
+export class CreatePettyCashReportDTO {
+    @IsString()
+    @IsNotEmpty()
+    readonly purpose: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly notes: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly credit: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly deposit: number;
+}
--- src/infrastructure/controllers/financial.controller.ts
+++ src/infrastructure/controllers/financial.controller.ts
@@ -1,13 +1,18 @@
+import { CreatePettyCashReportDTO } from "@data/dtos/pettyCash/create_petty_cash.dto";
 import { RequestWithUser } from "@data/interfaces/request.interface";
+import { CreatePettyCashReportUseCase } from "@domain/usecases/financial/create_petty_cash_for_property";
 import { GetPettyCashFinancialByPropertyUseCase } from "@domain/usecases/financial/get_petty_cash_report_by_property";
 import { GetStaffTravelExpenseUseCase } from "@domain/usecases/financial/get_staff_travel_expense_report_by_property";
 import { CheckTokenExpiry } from "@infrastructure/middlewares/token_expiry.middleware";
+import { ValidationMiddleware } from "@infrastructure/middlewares/validation.middleware";
 import {
   Authorized,
+  Body,
   Get,
   HttpCode,
   JsonController,
   Param,
+  Post,
   Req,
   UseBefore,
 } from "routing-controllers";
@@ -22,6 +27,9 @@ export class FinancialController {
   );
   private getStaffTravelExpenseUseCase = Container.get(
     GetStaffTravelExpenseUseCase,
+  );
+  private createPettyCashReportUseCase = Container.get(
+    CreatePettyCashReportUseCase,
   );
 
   @Get("/petty_cash/:property_id")
@@ -40,5 +48,12 @@ export class FinancialController {
     @Param("property_id") property_id: string,
   ) {
     return this.getStaffTravelExpenseUseCase.call(req, property_id);
+  }
+
+  @Post('/petty_cash/create/:property_id')
+  @UseBefore(ValidationMiddleware(CreatePettyCashReportDTO))
+  @HttpCode(201)
+  createPettyCashForProperty(@Req() req: RequestWithUser, @Param('property_id') property_id: string, @Body() data: CreatePettyCashReportDTO) {
+    return this.createPettyCashReportUseCase.call(req, property_id, data);
   }
 }
